f3d42cb2327f03b3b281729040c052f828d23e61
Ko W <lsbean62@gmail.com>
2021-12-29 18:52:07 +0900

    [add]リプライ機能追加

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,19 @@ import { Client, Message } from 'discord.js'
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"]})
 const TOKEN = 'OTI1NjI2MDI4MDI0NTYxNzI1.Ycv2ag.G4dbGSZjyoQTlpWTX3in9pXD8vM'
 
+client.on('ready', () => {
+  console.log("アルファくんが起きた")
+})
+
 client.on('messageCreate', (message: Message) => {
   if (message.author.bot) {
     return
   }
   if (message.content === "テスト") {
     message.channel.send("てすてす")
+  }
+  if (message.mentions.has(client.user?.id!)) {
+    message.reply("クエッ！")
   }
 })
 
