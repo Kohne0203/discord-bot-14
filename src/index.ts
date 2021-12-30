eb172e280ddd764c51e4229d21ed99696c5269b8
Ko W <lsbean62@gmail.com>
2021-12-30 13:47:16 +0900

    [add]ジョブルーレット機能を追加

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 require('dotenv').config()
 import { Client, Message } from 'discord.js'
+// import { BATTLE_JOB } from './static/constant'
 
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"]})
 const TOKEN = process.env.DISCORD_BOT_TOKEN
+// type BattleJob = typeof BATTLE_JOB[keyof typeof BATTLE_JOB]
 
 client.on('ready', () => {
   console.log("アルファくんが起きた")
@@ -13,11 +15,48 @@ client.on('messageCreate', (message: Message) => {
     return
   }
   if (message.content === "テスト") {
-    message.channel.send("てすてす")
+    sendText(message,"てすてす")
   }
   if (client.user && message.mentions.has(client.user.id)) {
     message.reply("クエッ！")
   }
+
+  if (message.content.match(/ジョブ/)) {
+    const msg = rouletteJobs()
+    sendText(message, msg)
+  }
 })
 
 client.login(TOKEN)
+
+function rouletteJobs () {
+  const jobAry  = [
+    'ナイト',
+    '戦士',
+    '暗黒騎士',
+    'ガンブレイカー',
+    '白魔導士',
+    '学者',
+    '占星術師',
+    '賢者',
+    'モンク',
+    '竜騎士',
+    '忍者',
+    '侍',
+    'リーパー',
+    '吟遊詩人',
+    '機工士',
+    '踊り子',
+    '黒魔道士',
+    '召喚士',
+    '赤魔道士',
+    '青魔導士'
+  ]
+  const random = Math.floor(Math.random() * jobAry.length)
+  const msg = "今日は" + jobAry[random] + "で行くクエッ！！"
+  return msg
+}
+
+function sendText (message:Message, text: string) {
+  message.channel.send(text)
+}
---
+++ src/static/constant.ts
@@ -0,0 +1,22 @@
+export const BATTLE_JOB = {
+  PALADIN: 'ナイト',
+  WARRIOR: '戦士',
+  DARK_KNIGHT: '暗黒騎士',
+  GUNBREAKER: 'ガンブレイカー',
+  WHITE_MAGE: '白魔導士',
+  SCHOLAR: '学者',
+  ASTROLOGIAN: '占星術師',
+  SAGE: '賢者',
+  MONK: 'モンク',
+  DRAGOON: '竜騎士',
+  NINJA: '忍者',
+  SAMURAI: '侍',
+  REAPER: 'リーパー',
+  BIRD: '吟遊詩人',
+  MACHINIST: '機工士',
+  DANCER: '踊り子',
+  BLACK_MAGE: '黒魔道士',
+  SUMMONER: '召喚士',
+  RED_MAGE: '赤魔道士',
+  BLUE_MAGE: '青魔導士'
+} as const
