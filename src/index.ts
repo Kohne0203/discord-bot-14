74d85a819e50f4d2144aa2f5600a70810c84b4ac
Ko W <kou.watanabe@mt-square.co.jp>
2022-05-14 01:01:54 +0900

    [add]xivapiからアイテム情報を取得する機能を追加

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ import { BATTLE_JOBS } from './static/constant'
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"]})
 const TOKEN = process.env.DISCORD_BOT_TOKEN
 // type BattleJob = typeof BATTLE_JOB[keyof typeof BATTLE_JOB]
+const XIVAPI = require('@xivapi/js')
+const xiv = new XIVAPI()
 
 client.on('ready', () => {
   console.log("アルファくんが起きた")
 })
 
-client.on('messageCreate', (message: Message) => {
+client.on('messageCreate', async (message: Message) => {
   if (message.author.bot) {
     return
   }
@@ -26,6 +28,16 @@ client.on('messageCreate', (message: Message) => {
     const msg = rouletteJobs()
     sendText(message, msg)
   }
+
+  if (message.content.match(/item/)) {
+    const content = message.content.split(' ')
+    const contentAry = content.splice(1, content.length)
+    const itemName = contentAry.join(' ')
+    console.log(itemName)
+    const id = await getId(itemName)
+    const msg = await getContent(id)
+    sendText(message, msg)
+  }
 })
 
 client.login(TOKEN)
@@ -38,4 +50,29 @@ function rouletteJobs () {
 
 function sendText (message:Message, text: string) {
   message.channel.send(text)
+}
+
+const getId = async (item: string) => {
+  // item名で検索する
+  let res = await xiv.search(item)
+  const id: number = res.Results.length ? res.Results[0].ID : 0
+
+  // return item ID
+  return id
+}
+
+async function getContent (id: number) {
+  let message: string = ''
+  const CONTENT = 'item'
+  await xiv.data.get(CONTENT, id).then((res: any)=> {
+    if (res.GameContentLinks.GilShopItem) {
+      message = res.Name_ja + '\nそのアイテムは店舗で買えるクエ！マケボ行ったらぼったくられるクエよ！！'
+    } else {
+      message = res.Name_ja + '\nそのアイテムは店舗で買えないクエ…マケボ行くしかないクエね…'
+    }
+  }).catch(() => {
+    message = 'よくわかんなかったクエ…'
+  })
+  
+  return message
 }
