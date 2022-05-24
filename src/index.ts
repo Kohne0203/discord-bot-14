f9338a3d5e314d53f8663d89e3f3a62fff2d5c34
Ko W <lsbean62@gmail.com>
2022-05-25 00:00:45 +0900

    [add]回答処理の実装、全体的にコメント追記

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
-require('dotenv').config()
 import { Client, Message } from 'discord.js'
-import { BATTLE_JOBS } from './static/constant'
-// import { BATTLE_JOB } from './static/constant'
+import { BATTLE_JOBS, ITEM, LODESTONE } from './static/constant'
+import i18next from 'i18next'
+import enJson from './static/locales/en.json'
+import jaJson from './static/locales/ja.json'
+
+// 多言語対応の設定
+i18next.init({
+  lng: 'en',
+  debug: true,
+  resources: {
+    en: { translation: enJson },
+    ja: { translation: jaJson }
+  }
+})
 
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"]})
 const TOKEN = process.env.DISCORD_BOT_TOKEN
-// type BattleJob = typeof BATTLE_JOB[keyof typeof BATTLE_JOB]
 const XIVAPI = require('@xivapi/js')
 const xiv = new XIVAPI()
+const Discord = require(`discord.js`)
+const { google } = require('googleapis')
+const customSearch = google.customsearch("v1")
 
+// 起動時にメッセージをログ出力
 client.on('ready', () => {
   console.log("アルファくんが起きた")
 })
@@ -17,9 +31,8 @@ client.on('messageCreate', async (message: Message) => {
   if (message.author.bot) {
     return
   }
-  if (message.content === "テスト") {
-    sendText(message,"てすてす")
-  }
+
+  // メンションに応じる
   if (client.user && message.mentions.has(client.user.id)) {
     message.reply("クエッ！")
   }
@@ -30,49 +43,90 @@ client.on('messageCreate', async (message: Message) => {
   }
 
   if (message.content.match(/item/)) {
-    const content = message.content.split(' ')
-    const contentAry = content.splice(1, content.length)
-    const itemName = contentAry.join(' ')
-    console.log(itemName)
-    const id = await getId(itemName)
-    const msg = await getContent(id)
-    sendText(message, msg)
+    // 検索キーワードに対して回答を送信する
+    const searchWord = message.content.replace('item ', '')
+    const itemId = await getItemId(i18next.t(searchWord))
+    const answer = await getAnswer(itemId)
+    sendText(message, answer)
+
+    // Google検索結果の埋め込みリンクを送信する
+    const searchResult = await searchGoogle(searchWord + LODESTONE)
+    const embed = new Discord.MessageEmbed()
+      .setTitle(searchResult.title)
+      .setURL(searchResult.link)
+      .addField(searchResult.snippet, searchResult.htmlFormattedUrl)
+      .setThumbnail(searchResult.pagemap.cse_thumbnail[0].src)
+      .setTimestamp()
+    
+    message.channel.send({ embeds: [embed] })
   }
 })
 
 client.login(TOKEN)
 
+
+/**
+ *  botのメッセージを送信する
+ */
+function sendText (message:Message, text: string) {
+  message.channel.send(text)
+}
+
+/**
+ *  ランダムにジョブ名を返す
+ */
 function rouletteJobs () {
   const random = Math.floor(Math.random() * BATTLE_JOBS.length)
   const msg = "今日は" + BATTLE_JOBS[random] + "で行くクエッ！！"
   return msg
 }
 
-function sendText (message:Message, text: string) {
-  message.channel.send(text)
-}
-
-const getId = async (item: string) => {
+/**
+ *　検索したキーワードからアイテムのIDを取得する
+ *  @param string itemName
+ */
+const getItemId = async (itemName: string) => {
   // item名で検索する
-  let res = await xiv.search(item)
+  let res = await xiv.search(itemName)
   const id: number = res.Results.length ? res.Results[0].ID : 0
 
   // return item ID
   return id
 }
 
-async function getContent (id: number) {
+/**
+ *　アルファくんの返答メッセージを作成する
+ *  @param number id
+ */
+async function getAnswer (id: number) {
+  // 返答用のメッセージを用意
   let message: string = ''
-  const CONTENT = 'item'
-  await xiv.data.get(CONTENT, id).then((res: any)=> {
+  // xivapiからアイテムデータを取得する
+  await xiv.data.get(ITEM, id).then((res: any)=> {
+    // GilShopItem内の要素の有無でメッセージを分岐する
     if (res.GameContentLinks.GilShopItem) {
-      message = res.Name_ja + '\nそのアイテムは店舗で買えるクエ！マケボ行ったらぼったくられるクエよ！！'
+      message = res.Name_ja + '\nそのアイテムは店舗で' + res.PriceMid +　'ギルで買えるクエ！マケボ行ったらぼったくられるクエよ！！'
     } else {
       message = res.Name_ja + '\nそのアイテムは店舗で買えないクエ…マケボ行くしかないクエね…'
     }
   }).catch(() => {
+    // キーワードでうまく検索できなかった場合のエラーハンドリング
     message = 'よくわかんなかったクエ…'
   })
   
   return message
+}
+
+/**
+ *　検索キーワードでgoogle検索APIを叩く
+ *  @param string keyword
+ */
+async function searchGoogle(keyword: string) {
+  const result = await customSearch.cse.list({
+    auth: process.env.GOOGLE_API_KEY,
+    cx: process.env.SEARCH_ENGINE_ID,
+    q: keyword
+  })
+
+  return result.data.items[0]
 }
